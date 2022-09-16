29b49a1366a87a00d41869c1b697c08f0e4261f7
Laszlo Simon <98slaci@gmail.com>
2022-09-16 13:41:55 +0200

    clear classes

--- tile.py
+++ tile.py
@@ -9,40 +9,46 @@ class Tile(pygame.sprite.Sprite):
         self.image = pygame.Surface((size, size))
         self.rect = self.image.get_rect(topleft = pos)
         self.pos = pygame.math.Vector2(self.rect.topleft)
-    
+
     def update(self, shift, dt) -> None:
         self.pos.x += shift * dt
         self.rect.x = round(self.pos.x)
-        self.wave(dt)
     
+class Solid(Tile):
+    def __init__(self, pos, size):
+        super().__init__(pos, size)
+
+class ShapeLess(Tile):
+    def __init__(self, pos, size):
+        super().__init__(pos, size)
+
     def wave(self, dt) -> None:
         self.pos.y += math.sin(self.animation) * 20 * dt
         self.rect.y = round(self.pos.y)
         self.animation += 0.05
 
-class Ground(Tile):
+    def update(self, shift, dt) -> None:
+        self.pos.x += shift * dt
+        self.rect.x = round(self.pos.x)
+        self.wave(dt)
+
+class Ground(Solid):
     def __init__(self, pos, size):
         super().__init__(pos, size)
         image = pygame.image.load("imgs/wall.jpg").convert_alpha()
         self.image.blit(image, (0, 0))
-    
-    def wave(self, dt) -> None:
-        pass
-    
-class Water(Tile):
+
+class Ice(Solid):
+    def __init__(self, pos, size):
+        super().__init__(pos, size)
+        self.image.fill('lightblue')
+
+class Water(ShapeLess):
     def __init__(self, pos, size):
         super().__init__(pos, size)
         self.image.fill("blue")
 
-class Lava(Tile):
+class Lava(ShapeLess):
     def __init__(self, pos, size):
         super().__init__(pos, size)
         self.image.fill('red')
-
-class Ice(Tile):
-    def __init__(self, pos, size):
-        super().__init__(pos, size)
-        self.image.fill('lightblue')
-    
-    def wave(self, dt) -> None:
-        pass
